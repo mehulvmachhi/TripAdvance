c3606347a1c89ca5160e09d1c5bcc728ea86541d
mehulvmachhi <mehulvmachhi@gmail.com>
2020-03-15 16:20:29 +0530

    add title of index page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>TRIP</title>
+        <title>TRIP - Login/SignUp</title>
         <link type="text/css" rel='stylesheet' href='assests/css/fontone.css'>
         <link type="text/css" rel='stylesheet' href='assests/css/fonttwo.css'>
         <link type="text/css" rel="stylesheet" href="assests/css/font-awesome.min.css">
